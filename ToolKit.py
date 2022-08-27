ef4c2282651182aa328ff70632eb276da04e96b7
Sapphire-code <85494077+Sapphire-code@users.noreply.github.com>
2022-08-27 12:02:05 +0800

    Update ToolKit.py

--- ToolKit.py
+++ ToolKit.py
@@ -123,28 +123,29 @@ class maths():
 
         def divide(self, num1, num2):
             print(num1 / num2)
-
-class network():
-    def start_netsh(self):
-        try:
-            os.startfile("C:\\Windows\\System32\\netsh.exe")
-        except FileNotFoundError:
-            print("Error, netsh.exe was not found")
-            input("Press Enter To Exit...")
-            exit()
+#This class is currently useless and isn't properly capable yet. functions are broken/don't work properly.
+# to use this class uncomment the lines below to use it. be warned however that they might not work as you expect.
+#class network():
+#    def start_netsh(self):
+#        try:
+#            os.startfile("C:\\Windows\\System32\\netsh.exe")
+#       except FileNotFoundError:
+#            print("Error, netsh.exe was not found")
+#            input("Press Enter To Exit...")
+#            exit()
     
 
 # using netsh to get wifi passwords on the system
-    def explorenet_PromptCli(self):
-        os.system("cls")
-        os.system("netsh wlan show profiles")
-        wifi = input("Which Wifi's password do you want?: ")
-        os.system('netsh wlan show profiles name="' + wifi + '" key=clear')
-        input("Press Enter To Exit...")
+#    def explorenet_PromptCli(self):
+#        os.system("cls")
+#        os.system("netsh wlan show profiles")
+#        wifi = input("Which Wifi's password do you want?: ")
+#        os.system('netsh wlan show profiles name="' + wifi + '" key=clear')
+#        input("Press Enter To Exit...")
     
-    def downloadnet(self, folderPath):
-        print("this function requires admin privilages please make sure you have admin")
-        os.system(f"netsh wlan export profile folder={folderPath} key=clear")
+#    def downloadnet(self, folderPath):
+#        print("this function requires admin privilages please make sure you have admin")
+#        os.system(f"netsh wlan export profile folder={folderPath} key=clear")
 
 
 
